5127373fff4099b8dd63608040ceb0601861341d
Akayneg <azizovakbarzon618@gmail.com>
2023-11-25 13:36:29 +0300

    Cumculator 1.2 Succeeded

--- app/src/main/java/com/example/cumculator_12/MainActivity.kt
+++ app/src/main/java/com/example/cumculator_12/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.example.cumculator_12
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
 
-class MainActivity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var workingView: TextView
+    private lateinit var resultView: TextView
+
+    private var input: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        workingView = findViewById(R.id.workingView)
+        resultView = findViewById(R.id.resultView)
     }
 
-    fun allClearButton(view: View) {}
-    fun equalsButton(view: View) {}
-    fun eraseButton(view: View) {}
-    fun symbolAction(view: View) {}
-    fun numberAction(view: View) {}
+
+    fun onNumberClick(view: View) {
+        val button = view as Button
+        input += button.text
+        updateWorkingView()
+    }
+
+    fun onSymbolClick(view: View) {
+        val button = view as Button
+        input += " " + button.text + " "
+        updateWorkingView()
+    }
+
+    fun onClearClick(view: View) {
+        input = ""
+        updateWorkingView()
+    }
+
+    fun onEraseClick(view: View) {
+        if (input.isNotEmpty()) {
+            input = input.substring(0, input.length - 1)
+            updateWorkingView()
+        }
+    }
+
+
+    fun onEqualsClick(view: View) {
+        try {
+            val result = evaluateExpression(input)
+            resultView.text = result.toString()
+        } catch (e: Exception) {
+            resultView.text = "Error"
+            e.printStackTrace()
+        }
+    }
+
+    private fun evaluateExpression(expression: String): Double {
+        try {
+            val result = ExpressionBuilder(expression).build().evaluate()
+            return result
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    private fun updateWorkingView() {
+        workingView.text = input
+    }
 }
